b0b4f16f5d04fdd2397be13094c3983bfcb67fe2
shoaib_raza_043 <mohdshoaib7379851089@gmail.com>
2025-07-02 23:49:09 +0530

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Student Feedback System</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="topHeader">
+	MOH SHOAIB PROJECTS<br />
+    <span class="tag">STUDENT FEEDBACK SYSTEM</span>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+<div id="content" align="center">
+<br>
+<br>
+<span class="SubHead">Student Feedback Step I</span>
+<form method="post" action="feedback_step_2.php" >
+<div id="table">
+	
+    
+    
+    <div class="tr">
+		<div class="td">
+        	<label>Roll No : </label>
+        </div>
+        <div class="td">
+                <input type="number" name="roll">
+        </div>
+    </div>
+</div>
+		
+        <div class="tdd">
+        	<input type="button" onClick="window.location='index.php'" value="BACK">&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="NEXT" />
+        </div>
+    
+    <br>
+</div>
+</form>
+<br>
+
+</body>
+</html>
